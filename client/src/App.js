f86faf1346e615e4428632c8cb141421df9a4e70
Marco Lin <pohanlin0605@gmail.com>
2022-08-20 11:02:44 +0800

    style: add themeProvide

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react'
+import { ThemeProvider } from '@emotion/react'
 import Login from './Login';
 import Dashboard from './Dashboard';
 
 const code = new URLSearchParams(window.location.search).get('code')
 
+const theme = {
+  dark: {
+    backgroundColor: '#191414',
+    albumBackgroundColor: '#181818',
+    buttonBackgroundColor: '#1DB954',
+    boxShadow:
+      '0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)',
+    titleColor: '#ffffff',
+    textColor: '#b3b3b3',
+  },
+  light: {
+    backgroundColor: '#ffffff',
+    albumBackgroundColor: '#f4f4f4',
+    boxShadow: '0 1px 3px 0 #999999',
+    titleColor: '#191414',
+    textColor: '#1c1c18',
+  }
+}
+
 function App() {
+  const [currentTheme, setCurrentTheme] = useState('dark')
   return (
-    code ? <Dashboard code={code}/> : <Login />
+    <ThemeProvider theme={theme[currentTheme]}>
+      {code ? <Dashboard code={code} currentTheme={currentTheme}/> : <Login currentTheme={currentTheme}/>}
+    </ThemeProvider> 
   )
 }
 
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,16 +1,38 @@
-import { Button } from 'react-bootstrap'
 import React from "react"
 import styled from '@emotion/styled'
+import SpotifyLogo from './images/Spotify_Logo_RGB_Green.png'
 
 const Container = styled.div`
   min-height: 100vh;
+  background-color: ${({ theme }) => theme.backgroundColor};
+`
+
+const Button = styled.a`
+  background-color: ${({ theme }) => theme.buttonBackgroundColor};
+  border-radius: 8px;
+  padding: 8px 16px;
+  margin-top: 32px;
+  border: none;
+  font-size: 16px;
+  color: ${({ theme }) => theme.titleColor};
+  text-decoration: none;
+  
+  &:hover {
+    color: ${({ theme }) => theme.titleColor};
+  }
+`
+
+const Logo = styled.img`
+  max-width: 400px;
+  margin-bottom: 32px;
 `
 
 export default function Login () {
   const url = process.env.REACT_APP_AUTH_URL
   return (
-    <Container className='d-flex justify-content-center align-items-center'>
-      <Button className='btn-success' href={url}>
+    <Container className='d-flex flex-column justify-content-center align-items-center'>
+      <Logo src={SpotifyLogo}/>
+      <Button href={url}>
         Login Spotify
       </Button>
     </Container>
